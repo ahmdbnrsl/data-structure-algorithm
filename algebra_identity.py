6723f62a39bc2866f56db7a14c50e18fd66caaef
ahmdbnrsl <bruceeeelyy@gmail.com>
2025-06-22 07:05:39 +0700

    feat: x + 1/x

---
+++ algebra_identity.py
@@ -0,0 +1,17 @@
+def identity(o, c):
+    if c <= 1: return o
+    
+    temp = 0
+    left = 0
+    for i in range(2, c + 1):
+        if i == 2:
+            temp = o * o - 2
+            left = o
+            continue
+        temp_left = temp
+        temp = temp * o - left
+        left = temp_left
+    
+    return temp
+
+print(identity(2, 99))
